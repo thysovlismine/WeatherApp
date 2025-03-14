dc7026c57a83b8a57fa541a0f086aec748e4b115
Anon <@>
2025-03-14 16:00:24 +0100

    async data fetching

--- main.cpp
+++ main.cpp
@@ -1,19 +1,31 @@
 #include <wx/wx.h>
+#include <wx/thread.h>
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
+#include <iostream>
+#include <thread>
 
 using json = nlohmann::json;
 
-// Main Frame
-class MyFrame : public wxFrame {
+// Custom event for thread-safe UI updates
+wxDECLARE_EVENT(wxEVT_FETCH_COMPLETE, wxThreadEvent);
+wxDEFINE_EVENT(wxEVT_FETCH_COMPLETE, wxThreadEvent);
+
+//================================================================
+#pragma region WindowMain
+
+class WindowMain : public wxFrame {
 public:
-    MyFrame() : wxFrame(nullptr, wxID_ANY, "REST API Data Fetcher 2", wxDefaultPosition, wxSize(600, 400)) {
+    WindowMain() : wxFrame(nullptr, wxID_ANY, "REST API Data Fetcher 2", wxDefaultPosition, wxSize(600, 400)) {
+        
+        // GUI Elements
         panel = new wxPanel(this);
         textCtrl = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(10, 10), wxSize(560, 300), wxTE_MULTILINE | wxTE_READONLY);
         fetchButton = new wxButton(panel, wxID_ANY, "Fetch Data", wxPoint(10, 320), wxSize(100, 30));
 
-        // Event binding
-        fetchButton->Bind(wxEVT_BUTTON, &MyFrame::OnFetchData, this);
+        // Events
+        fetchButton->Bind(wxEVT_BUTTON, &WindowMain::OnFetchData, this);
+        Bind(wxEVT_FETCH_COMPLETE, &WindowMain::OnFetchComplete, this);
     }
 
 private:
@@ -22,29 +34,46 @@ private:
     wxButton* fetchButton;
 
     void OnFetchData(wxCommandEvent& event) {
+        // Run the fetch operation in a separate thread
+        std::thread(&WindowMain::FetchData, this).detach();
+    }
+
+    void FetchData() {
         std::string url = "https://jsonplaceholder.typicode.com/posts/1";
         cpr::Response r = cpr::Get(cpr::Url{url});
 
+        wxThreadEvent evt(wxEVT_FETCH_COMPLETE);
         if (r.status_code == 200) {
             json jsonData = json::parse(r.text);
             std::string formattedData = "Title: " + jsonData["title"].get<std::string>() + "\n\n" +
                                         "Body: " + jsonData["body"].get<std::string>();
-            textCtrl->SetValue(formattedData);
+            evt.SetString(formattedData);
         } else {
-            textCtrl->SetValue("Failed to fetch data. HTTP Status Code: " + std::to_string(r.status_code));
+            evt.SetString("Failed to fetch data. HTTP Status Code: " + std::to_string(r.status_code));
         }
+        wxQueueEvent(this, evt.Clone());
+    }
+
+    void OnFetchComplete(wxThreadEvent& event) {
+        textCtrl->SetValue(event.GetString());
     }
 };
 
-// wxWidgets Application
+#pragma endregion
+
+//================================================================
+#pragma region Setup
+
 class MyApp : public wxApp {
 public:
     virtual bool OnInit() {
-        MyFrame* frame = new MyFrame();
-        frame->Show();
+        WindowMain* w_main = new WindowMain();
+        w_main->Show();
         return true;
     }
 };
 
-// wxWidgets Entry Point
+// Target that class
 wxIMPLEMENT_APP(MyApp);
+
+#pragma endregion
