1057317691108f8708df9b2ad48dc098719b8373
Anon <@>
2025-03-14 19:12:47 +0100

    additional checks for the base Panel class

--- src/Panel.cpp
+++ src/Panel.cpp
@@ -3,11 +3,14 @@
 Panel::Panel(Panel* origin){
     //assigns and hides a pervious panel
     _origin = origin;
-    _origin->panel->Hide();
+    //hides a pervious panel (if this is not the main one)
+    if(_origin != nullptr)
+        _origin->panel->Hide();
 }
 
 Panel::~Panel(){
     //since this object is being deleted, the object that rely on it should be too.
-    //show the previous panel
-    _origin->panel->Show();
+    //show the previous panel (if this is not the main one)
+    if(_origin != nullptr) 
+        _origin->panel->Show();
 }
