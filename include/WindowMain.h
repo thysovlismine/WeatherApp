1ffb5299416f2130f0a4e274a6b82f9f255bec93
Anon <@>
2025-03-27 18:11:06 +0100

    docs for WindowMain.h

--- include/WindowMain.h
+++ include/WindowMain.h
@@ -1,18 +1,36 @@
-// WindowMain.h
-#pragma once
-#ifndef WINDOWMAIN_H
-#define WINDOWMAIN_H
+/**
+ * @file WindowMain.h
+ * @brief Defines the main application window class.
+ */
 
-#include <wx/wx.h>
-#include "Panel.h"
-
-class WindowMain : public wxFrame {
-    public:
-    WindowMain();
-
-    //main panel
-    Panel* panel_start;
-    
-};
-
-#endif // WINDOWMAIN_H
+ #pragma once
+ #ifndef WINDOWMAIN_H
+ #define WINDOWMAIN_H
+ 
+ #include <wx/wx.h>
+ #include "Panel.h"
+ 
+ /**
+  * @class WindowMain
+  * @brief The main application window.
+  * 
+  * This class serves as the primary frame for the application, initializing 
+  * the main panel and setting up the GUI layout.
+  */
+ class WindowMain : public wxFrame {
+ public:
+     /**
+      * @brief Constructs the main application window.
+      * 
+      * Initializes the main frame and sets up the starting panel.
+      */
+     WindowMain();
+ 
+     /// @name Main Panel
+     /// @{
+     Panel* panel_start; ///< Pointer to the initial panel displayed in the main window.
+     /// @}
+ };
+ 
+ #endif // WINDOWMAIN_H
+ 
